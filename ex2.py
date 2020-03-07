5525c3315e36c58921b7852693b9c5ebdb66b07d
justinmraj <justinmraj@gmail.com>
2020-03-07 20:29:52 +0530

    Create ex2.py
    ex2 - comments

---
+++ ex2.py
@@ -0,0 +1,8 @@
+# A comment, makes the program readable
+# Anything after the # is ignored by Python
+
+print("This is my print statement") # and the comment after is ignored
+
+# You can also use comment to "disable" a piece of code
+# print("This is the commented piece of code and will not be executed")
+print("This print statement will run")
