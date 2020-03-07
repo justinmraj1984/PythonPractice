5918b6335f14820366a49b392c627128ebb24780
justinmraj <justinmraj@gmail.com>
2020-03-07 22:26:07 +0530

    ex3 - numbers & math

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PythonPractice
  Learn Python Programming
+
+- Ex 1 - Print Statement
+- Ex 2 - Comments
+- Ex 3 - Numbers & Math
---
+++ ex3.py
@@ -0,0 +1,16 @@
+#PEMDAS - Parantheses, Exponents, Multiplication, Division, Addition, Subtraction
+
+print("I will count my Chicken:")
+print("Hens", 25+30/6)
+print("Rooster", 100-25+3%4)
+
+print("I will count my eggs:")
+print(3+2+1-5+4%2-1/4+6) 
+
+print("Is it true that 3+2 < 5-7 ?")
+print(3+2 < 5-7)
+
+print("Value of 3+2 :", 3+2)
+print("Value of 5-7 :", 5-7)
+
+print("Oh, thats why 3+2 < 5-7 is False")
